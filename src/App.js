dc04ac65f9ea3d3f06439b8c1d54ad19bf05b66a
GoktugYmsk <gogtug033@outlook.com>
2025-02-04 14:58:37 +0300

    added router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Content from "./components/Content";
+
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Content />} />
+            {/* <Route path="/upload" element={<PhotoUpload />} />
+            <Route path="/contest/:id" element={<ContestDetail />} />
+            <Route path="/profile" element={<UserProfile />} />
+
+            <Route path="/admin/*" element={<AdminDashboard />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Content() {
+function Footer() {
   return <div></div>;
 }
 
-export default Content;
+export default Footer;
